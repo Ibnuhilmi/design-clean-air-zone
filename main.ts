e37de9b9691fb1c1565f1188c1e712dbfc30ad8b
Ibnuhilmi <132650202+Ibnuhilmi@users.noreply.github.com>
2025-05-06 10:41:18 +0700

    Perbarui main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
-// Otomasi penanaman pohon
-player.onChat("Hijaukan", function(){
-    for (let i = 0; i < 5; i++){
-        agent.setItem (OAK_SAPLING, 64, 1)
-        agent.move (FORWARD, 1)
-        agent.place (DOWN)
+/**
+ * Otomasi penanaman pohon
+ * 
+ * player.onChat("Hijaukan", function () {
+ * 
+ * for (let index = 0; index < 5; index++) {
+ * 
+ * agent.setItem(OAK_SAPLING, 64, 1)
+ * 
+ * agent.move(FORWARD, 1)
+ * 
+ * agent.place(DOWN)
+ * 
+ * }
+ * 
+ * })
+ */
+player.onChat("Ukur_Polusi", function () {
+    if (Polusi > 0) {
+        player.say("Polusi: " + Polusi)
+    } else {
+        player.say("Udara Bersih!")
     }
 })
+let Polusi = 0
+// Polusi lagi
+Polusi = 20
